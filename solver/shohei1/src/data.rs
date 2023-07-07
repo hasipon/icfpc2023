8538d91a707bd0e26d411c3f78b2874d8b2730dd
shohei909 <909mmsphere@gmail.com>
2023-07-08 05:25:52 +0900

    solver shohei1

---
+++ solver/shohei1/src/data.rs
@@ -0,0 +1,30 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Problem {
+	pub room_width: f64,
+	pub room_height: f64,
+	pub stage_width: f64,
+	pub stage_height: f64,
+	pub stage_bottom_left:(f64, f64),
+	pub musicians:Vec<i32>,
+	pub attendees:Vec<Attendee>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Attendee {
+    pub x:f64,
+    pub y:f64,
+    pub tastes:Vec<f64>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Answer {
+    pub placements:Vec<Point>
+}
+
+#[derive(Serialize, Deserialize, Debug, Copy, Clone)]
+pub struct Point {
+    pub x:f64,
+    pub y:f64,
+}
