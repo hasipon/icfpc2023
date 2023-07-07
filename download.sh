c0504991070b462ff7f0a097899a094998fc92f9
shohei909 <909mmsphere@gmail.com>
2023-07-08 05:49:53 +0900

    download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(seq 1 45); do \
+for i in $(seq 1 55); do \
   echo ${i}
   curl "https://api.icfpcontest.com/problem?problem_id=${i}" | jq -r .Success > "problem.json/${i}.json"
 done
