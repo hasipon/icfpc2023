772fda05924e5871e44e662e373e36ed85c00fb0
hasipon <hasipon@users.noreply.github.com>
2023-07-09 04:27:47 +0000

    11-hasi10

---
+++ solver/hasi10/c.cpp
@@ -0,0 +1,211 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <cmath>
+#include <complex>
+#include <algorithm>
+using namespace std;
+
+struct Attendee {
+    double x;
+    double y;
+    vector<double> tastes;
+};
+
+struct Pillar {
+    double x;
+    double y;
+    double r;
+};
+
+struct Problem {
+    double roomWidth;
+    double roomHeight;
+    double stageWidth;
+    double stageHeight;
+    double stageBottom;
+    double stageLeft;
+    vector<int> musicians;
+    vector<Attendee> attendees;
+    vector<Pillar> pillars;
+};
+
+bool isBlocked(double x0, double y0, double x1, double y1, double x2, double y2, double radius) {
+    complex<double> p0(x0, y0), p1(x1, y1), p2(x2, y2);
+    if (real(conj(p1 - p0) * (p2 - p0)) < 0) return false;
+    if (real(conj(p0 - p1) * (p2 - p1)) < 0) return false;
+    double t = real(conj(p2 - p0) * (p0 - p1)) / norm(p0 - p1);
+    return abs(p2 - (p0 + (p0 - p1) * t)) < radius;
+}
+
+pair<bool, long long> calcScore(const Problem& problem, vector<pair<double, double>> placements) {
+    if (placements.size() != problem.musicians.size()) {
+        return {false, 0};
+    }
+    for (unsigned i = 0; i < placements.size(); i++) {
+        auto [x, y] = placements[i];
+        if (!(
+                problem.stageBottom + 10 <= y && y <= problem.stageBottom + problem.stageHeight - 10 &&
+                problem.stageLeft + 10 <= x && x <= problem.stageLeft + problem.stageWidth - 10
+        )) {
+            return {false, 0};
+        }
+        for (unsigned j = 0; j < i; j++) {
+            auto [x2, y2] = placements[j];
+            if ((x - x2) * (x - x2) + (y - y2) * (y - y2) < 100) {
+                return {false, 0};
+            }
+        }
+    }
+    vector<double> factor(placements.size(), 1);
+    if (!problem.pillars.empty()) {
+        for (unsigned i = 0; i < placements.size(); i++) {
+            auto [x1, y1] = placements[i];
+            for (unsigned j = 0; j < placements.size(); j++) {
+                if (j != i && problem.musicians[i] == problem.musicians[j]) {
+                    auto [x2, y2] = placements[j];
+                    auto d2 = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+                    factor[i] += 1 / sqrt(d2);
+                }
+            }
+        }
+    }
+    double score = 0;
+    for (auto& a : problem.attendees) {
+        for (unsigned i = 0; i < placements.size(); i++) {
+            auto [x, y] = placements[i];
+            for (auto pr : problem.pillars) {
+                if (isBlocked(a.x, a.y, x, y, pr.x, pr.y, pr.r)) {
+                    goto next;
+                }
+            }
+            for (unsigned j = 0; j < placements.size(); j++) {
+                if (i != j) {
+                    auto [x2, y2] = placements[j];
+                    if (isBlocked(a.x, a.y, x, y, x2, y2, 5)) {
+                        goto next;
+                    }
+                }
+            }
+            {
+                auto d2 = (a.x - x) * (a.x - x) + (a.y - y) * (a.y - y);
+                auto s = (long long)ceil(1000000 * a.tastes[problem.musicians[i]] / d2);
+                if (factor[i] > 1) {
+                    s = (long long)ceil(factor[i] * s); // NOLINT(cppcoreguidelines-narrowing-conversions)
+                }
+                score += s; // NOLINT(cppcoreguidelines-narrowing-conversions)
+            }
+            next:;
+        }
+    }
+    return {true,score};
+}
+
+bool checkPlacements(double x, double y, const vector<pair<double, double>>& placements) {
+    for (auto [x2, y2] : placements) {
+        if ((x - x2) * (x - x2) + (y - y2) * (y - y2) < 100) {
+            return false;
+        }
+    }
+    return true;
+}
+
+pair<int, int> makeRandPos(const Problem& problem, const vector<pair<double, double>>& placements) {
+    for (;;) {
+        double x0 = problem.stageLeft + 10 + rand() % ((int)problem.stageWidth - 19);
+        double y0 = problem.stageBottom + 10 + rand() % ((int)problem.stageHeight - 19);
+        if (checkPlacements(x0, y0, placements)) return {x0, y0};
+    }
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        cerr << "Usage: " << argv[0] << " <input file>" << endl;
+        return 1;
+    }
+
+    Problem problem;
+    cin >> problem.roomWidth >> problem.roomHeight;
+    cin >> problem.stageWidth >> problem.stageHeight;
+    cin >> problem.stageLeft >> problem.stageBottom;
+    int musicianN, tasteN, attendeeN, pillarN;
+    cin >> musicianN >> tasteN;
+    problem.musicians.resize(musicianN);
+    for (auto& m : problem.musicians) {
+        cin >> m;
+    }
+    cin >> attendeeN;
+    problem.attendees.resize(attendeeN);
+    for (auto& a : problem.attendees) {
+        cin >> a.x >> a.y;
+        a.tastes.resize(tasteN);
+        for (auto& t : a.tastes) {
+            cin >> t;
+        }
+    }
+    cin >> pillarN;
+    problem.pillars.resize(pillarN);
+    for (auto& p : problem.pillars) {
+        cin >> p.x >> p.y >> p.r;
+    }
+
+    ifstream fin(argv[1]);
+    int nPlacements;
+    fin >> nPlacements;
+    vector<pair<double, double>> planPos(nPlacements);
+    for (auto& p : planPos) {
+        fin >> p.first >> p.second;
+    }
+    vector<pair<int, int>> tasteCnt;
+    vector<vector<long long>> scoreTable;
+    {
+        int taste, nTaste;
+        while (fin >> taste >> nTaste) {
+            tasteCnt.emplace_back(taste, nTaste);
+            scoreTable.emplace_back(nPlacements);
+            for (auto& s : scoreTable.back()) {
+                fin >> s;
+            }
+        }
+    }
+
+    vector<int> tastes {104,25,47,72,72,39,71,55,75,60,10,36,106,80,108,34,88,94,94,88,58,70,79,79,79,107,28,53,63,37,65,90,30,7,101,7,42,42,42,12,90,87,54,33,23,56,103,114,19,81,110,68,98,64,4,82,57,26,26,50,14,78,78,73,99,99,43,5,97,18,100,6,51,113,69,111,17,17,111,95,74,89,21,89,21,25,49,85,11,17,105,96};
+    map<int, vector<unsigned>> plan;
+    for (unsigned i = 0; i < tastes.size(); i++) {
+        plan[tastes[i]].push_back(i);
+    }
+
+    vector<pair<double, double>> placements(problem.musicians.size());
+    vector<pair<double, double>> used;
+    vector<unsigned> noPlan;
+    for (unsigned i = 0; i < problem.musicians.size(); ++ i) {
+        if (plan.count(problem.musicians[i]) && !plan[problem.musicians[i]].empty()) {
+            auto idx = plan[problem.musicians[i]].back();
+            plan[problem.musicians[i]].pop_back();
+            placements[i] = planPos[idx];
+            used.push_back(placements[i]);
+        } else {
+            noPlan.push_back(i);
+        }
+    }
+    for (auto i : noPlan) {
+        placements[i] = makeRandPos(problem, used);
+        used.push_back(placements[i]);
+    }
+
+    cout << "{\"placements\":[";
+    for (unsigned i = 0; i < placements.size(); i++) {
+        if (i > 0) cout << ",";
+        cout << "{\"x\":" << placements[i].first << ",\"y\":" << placements[i].second << "}";
+    }
+    cout << "]}" << endl;
+
+    auto [ok, score] = calcScore(problem, placements);
+    if (!ok) {
+        cerr << "Invalid placements" << endl;
+        return 1;
+    }
+    cerr << "score = " << score << endl;
+}
---
+++ solver/hasi10/run_c.sh
@@ -0,0 +1,1 @@
+./c $1.out < ../../problems.kyopro/$1.kyopro > ../../solutions/$1-hasi10.json
