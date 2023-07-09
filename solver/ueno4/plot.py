698f3010495d205f74b87ba0237f388598a3033d
Yuichiro Ueno <y1r.ueno@gmail.com>
2023-07-09 11:27:22 +0900

    ueno-4: consider pillar

--- solver/ueno4/plot.py
+++ solver/ueno4/plot.py
@@ -1,24 +1,36 @@
+import argparse
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("1-floor.csv", header=None, names=["score", "x", "y"])
 
-x = data["x"]
-x_max = np.max(x) // 10 + 1
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data")
+    args = parser.parse_args()
 
-y = data["y"]
-y_max = np.max(y) // 10 + 1
+    data = pd.read_csv(args.data, header=None, names=["score", "x", "y"])
 
-print(x_max, y_max)
+    x = data["x"]
+    x_max = np.max(x) // 10 + 1
 
-colormap = np.zeros((x_max, y_max), dtype=np.float32)
+    y = data["y"]
+    y_max = np.max(y) // 10 + 1
 
-for s, x, y in zip(data["score"], data["x"], data["y"]):
-    colormap[int(x // 10), int(y // 10)] = s
+    print(x_max, y_max)
 
-print(colormap)
+    colormap = np.zeros((x_max, y_max), dtype=np.float32)
 
-im1 = plt.imshow(colormap.T)
-plt.colorbar(im1)
-plt.show()
+    for s, x, y in zip(data["score"], data["x"], data["y"]):
+        colormap[int(x // 10), int(y // 10)] = s
+
+    print(colormap)
+
+    im1 = plt.imshow(colormap.T)
+    plt.colorbar(im1)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
--- solver/ueno4/run.sh
+++ solver/ueno4/run.sh
@@ -2,6 +2,6 @@
 
 for f in $(ls ../../problems.kyopro/*.kyopro);
 do
-    cat $f | ./a.out | tee $(basename $f .kyopro).csv
+    cat $f | ./a.out | tee $(basename $f .kyopro).csv &
 done
 wait
