ac3601a83d9f56bf1ecc1f01a4c20ad1652b51f6
Johniel <johniel.s.m@gmail.com>
2023-07-09 22:38:04 +0900

    j10-5

--- solver/johniel10/main.cpp
+++ solver/johniel10/main.cpp
@@ -684,8 +684,8 @@ pair<vector<pair<double, double>>, vec<double>> solveF(const Problem& problem)
 
   const int enabled = candidates.size();
 
-  for (int i = -20; i <= 20; ++i) {
-    for (int j = -20; j <= 20; ++j) {
+  for (int i = -10; i <= 10; ++i) {
+    for (int j = -10; j <= 10; ++j) {
       pair<double, double> p = bottomLeft;
       p.first += problem.stageWidth / 2.0;
       p.second += problem.stageHeight / 2.0;
--- solver/johniel10/run.sh
+++ solver/johniel10/run.sh
@@ -1,4 +1,4 @@
 for i in `seq 1 90`
 do
-	./a.out < ../../problems.kyopro/$i.kyopro > $i-johniel10-4.json 2> $i-johniel10.err &
+	./a.out < ../../problems.kyopro/$i.kyopro > $i-johniel10-5.json 2> $i-johniel10.err &
 done
