50c573fb4a3948be8728ef3094ec4b85c026e343
Your Name <you@example.com>
2023-07-10 07:56:54 +0000

    hoge

--- solver/shiota13/all_run.sh
+++ solver/shiota13/all_run.sh
@@ -1,6 +1,6 @@
 set -eux
 g++ -Ofast -std=c++14 main.cpp
-for i in `seq 2 90`
+for i in `seq 1 2`
 do
 	./a.out < ../../problems.kyopro/$i.kyopro > ../../solutions/${i}-shiota13.json
 	PROBLEM_ID=$i REPO_ROOT=/home/shiota/icfpc2023 cargo run --release --manifest-path ./shohei15-2/Cargo.toml > ../../solutions/$i-shiota-13-v2.json &
