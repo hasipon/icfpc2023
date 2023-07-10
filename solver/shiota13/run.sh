c416c6173ab2567ec7cf0589d6963600ae4eb9fe
Your Name <you@example.com>
2023-07-10 04:57:25 +0000

    fix

--- solver/shiota13/run.sh
+++ solver/shiota13/run.sh
@@ -1,6 +1,7 @@
-#! /bin/bash
+#!/bin/bash
 
 set -eux
+cd $REPO_ROOT/solver/shiota13
 g++ -Ofast -std=c++14 main.cpp
 ./a.out < $REPO_ROOT/problems.kyopro/${PROBLEM_ID}.kyopro > $REPO_ROOT/solutions/${PROBLEM_ID}-shiota13.json
 cd shohei15-2 && cargo run --release
