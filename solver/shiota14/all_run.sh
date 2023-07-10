5b956329b316ee4fdc32932e89a30b6341fbcafd
Your Name <you@example.com>
2023-07-10 09:49:18 +0000

    hoge

--- solver/shiota14/all_run.sh
+++ solver/shiota14/all_run.sh
@@ -1,8 +1,7 @@
 set -eux
-g++ -Ofast -std=c++14 main.cpp
+g++ -Ofast -std=c++17 main.cpp
 cargo build --release --manifest-path ./shohei15-2/Cargo.toml
-for i in `seq 1 1`
+for i in `seq 1 90`
 do
-	./a.out < ../../problems.kyopro/$i.kyopro > ../../solutions/${i}-shiota14.tmp
-	PROBLEM_ID=$i REPO_ROOT=/home/shiota/icfpc2023 ./shohei15-2/target/release/solver > ../../solutions/$i-shiota-14-v2.json &
+	bash -c "./a.out < ../../problems.kyopro/$i.kyopro > ../../solutions/${i}-shiota14.tmp; PROBLEM_ID=$i REPO_ROOT=/home/shiota/icfpc2023 ./shohei15-2/target/release/solver > ../../solutions/$i-shiota-14-v2.json" &
 done
